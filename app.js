f35d5c4f31209b18871f50ad5b90c56a93b5a100
Your Name <you@example.com>
2022-06-16 22:56:02 +0900

    2章

---
+++ app.js
@@ -0,0 +1,19 @@
+// let max = 20;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert("2を掛け続けて" + max + "を超えるのに必要だった回数は" + count + "回です");
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i
+}
+
+alert("1から10を足し続けると結果は" + num + "です");
