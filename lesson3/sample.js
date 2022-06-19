0b2ab61d8c9b89a378c5e43879a035023c8761ab
Your Name <you@example.com>
2022-06-19 17:25:06 +0900

    lesson3

---
+++ lesson3/sample.js
@@ -0,0 +1,29 @@
+// var nickname = "Taro"
+// console.log(nickname)
+// nickname = "Ichiro"
+// console.log(nickname)
+
+// let nickname = "Taro"
+// console.log(nickname)
+// nickname = "Ichiro"
+// console.log(nickname)
+
+// const nickname = "Taro"
+// console.log(nickname)
+// nickname = "Ichiro"
+// console.log(nickname)
+
+// 変数の巻き上げ
+// var str = "webcamp"
+
+// function foo(){
+//   console.log(str)
+//   var str = "dmm webcamp"
+//   console.log(str)
+// }
+
+// foo()
+
+for(let i = 0; i>10; i++){
+  console.log(i);
+}
