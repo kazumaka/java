9fc0a83076b5cade185d2ee80f4defc05fb9bbbb
Your Name <you@example.com>
2022-06-27 23:07:56 +0900

    lesson8

---
+++ lesson7/app.js
@@ -0,0 +1,12 @@
+// パターン1
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
